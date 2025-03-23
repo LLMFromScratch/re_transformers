e7d942044e89ef9ba26e69431658f39b5bde053e
ChengaFEI <cf482@cornell.edu>
2025-03-23 03:43:37 -0700

    Add modeling code for Albert

--- src/re_transformers/models/albert/modeling_albert.py
+++ src/re_transformers/models/albert/modeling_albert.py
@@ -1,5 +1,8 @@
-from configuration_albert import AlbertConfig
+import torch.nn as nn
+
+from re_transformers.models.albert.configuration_albert import AlbertConfig
 
 
 class AlbertTransformer(nn.Module):
-    pass
+    def __init__(self, config: AlbertConfig):
+        pass
