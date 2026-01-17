52c704fd88cdfd9407662000cc03538b87141e37
ChengaFEI <cf482@cornell.edu>
2026-01-17 09:18:36 -0800

    Add rotation and positional embedding functions to Qwen3 model

--- src/re_transformers/models/qwen3/modeling_qwen3.py
+++ src/re_transformers/models/qwen3/modeling_qwen3.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 import torch.nn as nn
 from transformers import Qwen3Config
@@ -39,6 +41,26 @@ class Qwen3MLP(nn.Module):
 
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
         return self.down_proj(self.act_fn(self.gate_proj(hidden_states)) * self.up_proj(hidden_states))
+
+
+def rotate_half(x: torch.Tensor) -> torch.Tensor:
+    x1 = x[..., :x.size(-1) // 2]
+    x2 = x[..., x.size(-1) // 2:]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotaty_pos_emb(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    cos: torch.Tensor,
+    sin: torch.Tensor,
+    unsqueeze_dim: int = 1
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+    q_embed = q * cos + rotate_half(q) * sin
+    k_embed = k * cos + rotate_half(k) * sin
+    return q_embed, k_embed
 
 
 # Questions
