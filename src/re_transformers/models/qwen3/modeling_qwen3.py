5f1cab6f60e39fce5f4ea40998bfe23934343228
ChengaFEI <cf482@cornell.edu>
2026-01-17 06:02:50 -0800

    Add Qwen3MLP class and update imports in modeling_qwen3.py

--- src/re_transformers/models/qwen3/modeling_qwen3.py
+++ src/re_transformers/models/qwen3/modeling_qwen3.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from transformers import Qwen3Config
+from transformers.activations import ACT2FN
 from transformers.integrations import use_kernel_forward_from_hub
 
 
@@ -21,3 +23,24 @@ class Qwen3RMSNorm(nn.Module):
 
     def extra_repr(self):
         return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
+
+
+class Qwen3MLP(nn.Module):
+    def __init__(self, config: Qwen3Config) -> None:
+        super().__init__()
+
+        self.gate_proj = nn.Linear(
+            config.hidden_size, config.intermediate_size, bias=False)
+        self.up_proj = nn.Linear(
+            config.hidden_size, config.intermediate_size, bias=False)
+        self.down_proj = nn.Linear(
+            config.intermediate_size, config.hidden_size, bias=False)
+        self.act_fn = ACT2FN[config.hidden_act]
+
+    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+        return self.down_proj(self.act_fn(self.gate_proj(hidden_states)) * self.up_proj(hidden_states))
+
+
+# Questions
+# 1. `Qwen3Config::keys_to_ignore_at_inference`
+# 2. `Qwen3Config::base_model_pp_plan`
